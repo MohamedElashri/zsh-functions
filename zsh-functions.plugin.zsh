c4873eec6ea2d5d20e120bb2d6b3a8d78f1da96f
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2021-10-28 17:59:47 +0000

    Clone any repo and cd into it

--- zsh-functions.plugin.zsh
+++ zsh-functions.plugin.zsh
@@ -93,5 +93,51 @@ reload() {
 }
 
       
+# Clone any repo and cd into it 
+clone() {
+    cloneUsage() {
+        echo "\n⚠️  Usage:\nclone <url*> <dir>\nclone <org*>/<repo*> <dir>\nclone <org*> <repo*> <dir>\n* denotes required arguments"
+    }
 
+    gitClone() {
+        CYAN="\033[1;36m"
+        NOCOLOR="\033[0m"
+        echo "🤖 Cloning $1"
+        git clone $2 $directory && cd $directory || (cloneUsage && return 1)
+        # If an error code was returned from the last command, return an error code
+        if [[ "$?" == 1 ]]; then
+            return 1
+        fi
+        echo "🚚 Moved to directory ${CYAN}$directory${NOCOLOR}"
+    }
+
+    if [[ -z $1 ]]; then
+        cloneUsage
+        return 1
+    fi
+
+    gitURL="$1"
+    gitURL="${gitURL%.git}" # Remove .git from the end of the git URL
+    if [[ $gitURL =~ ^git@ ]]; then
+        gitURL="$(echo $gitURL | sed 's/git@//')" # Remove git@ from the start of the git URL
+        org="$(echo $gitURL | sed 's/.*\://' | sed 's/\/.*//')" # Pull the org from the git URL
+        repo="$(echo $gitURL | sed 's/.*\///')" # Pull the repo name from the git URL
+        directory=${2:-"$repo"}
+        gitClone "$org/$repo" $1
+    elif [[ $gitURL =~ ^https?:// ]]; then
+        # Force SSH
+        github="$(echo $gitURL | cut -d'/' -f3)"
+        org="$(echo $gitURL | sed 's/.*\.com\///' | sed 's/\/.*//')" # Pull the org from the git URL
+        repo="$(echo $gitURL | sed 's/.*\.com\///' | sed 's/.*\///')" # Pull the repo name from the git URL
+        directory=${2:-"$repo"}
+        gitClone "$org/$repo" "git@$github:$org/$repo.git"
+    elif [[ ! -z $1 && ! -z $2 && "$1" != *\/*  ]]; then
+        directory=${3:-"$2"}
+        gitClone "$1/$2" "git@github.com:$1/$2.git" # Replace with GitHub Enterprise URL (if applicable)
+    else
+        repo="$(echo $1 | sed 's/.*\///')"
+        directory=${2:-"$repo"}
+        gitClone "$1" "git@github.com:$1.git" # Replace with GitHub Enterprise URL (if applicable)
+    fi
+}
         
